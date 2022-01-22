e27e3b54d2f9be2d14552344b318d06ff1e23de8
Brian Arnold <b.arnold09@gmail.com>
2022-01-22 04:39:23 -0500

    Assignment Complete

---
+++ index.js
@@ -0,0 +1,35 @@
+let a = 5;
+let b = 10;
+let n = 15
+
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a ,b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function increment(n) {
+    return (++n);
+}
+
+function decrement(n) {
+    return (--n);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
